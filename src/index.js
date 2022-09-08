0373010f90b95962245c3bfe73228f92fcbce957
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-09-08 08:16:46 +0300

    Completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  const newTaskForm  = document.getElementById('create-task-form');
+  const newTaskPriority = document.getElementById('new-tasl-priority');
+  const newTaskUI = document.getElementById('tasks');
+
+  newTaskForm.addEventListener('submit', createNewTask);
 });
+
+const createNewTask = e => {
+  e.preventDefault();
+
+  const newTaskDescription = document.getElementById('new-task-descrition');
+  const newTask = document.createElement('li');
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  e.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById('tasks').appendChild(task)
+}
+
